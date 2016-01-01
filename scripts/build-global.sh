1b4b4922a662b6d65d9bee723803955146c42994
Joseph Orbegoso Pea <joe@trusktr.io>
2015-12-31 16:24:55 -0800

    User should choose the global identifier that will be assigned to `window`.

--- scripts/build-global.sh
+++ scripts/build-global.sh
@@ -1,4 +1,5 @@
 # Here we use webpack to build a UMD (Universal Module Definition) module that
 # will put the exports of your package entrypoint onto the the global scope
 # (`window` in a browser, for example)
-webpack --progress --colors --output-library-target umd src/index.js global.js --output-library motor
+package_name='YOUR_PACKAGE_NAME_HERE' # <- modify this
+webpack --progress --colors --output-library-target umd src/index.js global.js --output-library $package_name
