63b0621763f89a086d33dab341ec42a2b88d83fc
Joseph Orbegoso Pea <joe@trusktr.io>
2016-01-15 22:02:43 -0800

    Remove dupe config from Webpack, let it come from .babelrc. Ignore don't process node_modules.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
-// These are the Webpack settings used when compiling the global version of the library.
+// These are the Webpack settings used when compiling the global version of the
+// library.
 
 module.exports = {
     resolve: {
@@ -10,42 +11,18 @@ module.exports = {
             {
                 test: /\.jsx?$/,
                 loader: 'babel',
-                //exclude: /node_modules/,
+                exclude: /node_modules/,
                 query: {
                     cacheDirectory: true,
 
-                    // ignore babelrc, which is used for some package.json scripts.
-                    babelrc: false,
+                    // ignore babelrc, which is used for some package.json
+                    // scripts.
+                    babelrc: true,
 
-                    presets: [
-                        'react' // for JSX syntax.
-                    ],
-
+                    // this is specific to Webpack, for making the global
+                    // build. The rest is located in .babelrc
                     plugins: [
-                        'transform-es2015-arrow-functions',
-                        'transform-es2015-block-scoped-functions',
-                        'transform-es2015-block-scoping',
-                        'transform-es2015-classes',
-                        'transform-es2015-computed-properties',
-                        'transform-es2015-destructuring',
-                        'transform-es2015-for-of',
-                        'transform-es2015-function-name',
-                        'transform-es2015-literals',
-                        'transform-es2015-object-super',
-                        'transform-es2015-parameters',
-                        'transform-es2015-shorthand-properties',
-                        'transform-es2015-spread',
-                        'transform-es2015-sticky-regex',
-                        'transform-es2015-template-literals',
-                        'transform-es2015-typeof-symbol',
-                        'transform-es2015-unicode-regex',
-                        'transform-regenerator',
-
-                        'transform-async-to-generator',
-
-                        'transform-es5-property-mutators',
-
-                        'transform-es2015-modules-umd', // this is for the global build.
+                        'transform-es2015-modules-commonjs',
                     ],
                 },
             },
